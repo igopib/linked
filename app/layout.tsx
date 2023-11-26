dfd01329259b74182eabd4670bfd7b4cb65c56a9
igopib <gs109690@gmail.com>
2023-11-26 01:44:39 -0600

    chore: shadcn ui library

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className='w-full h-screen bg-red-500'>{children}</body>
     </html>
   )
 }
